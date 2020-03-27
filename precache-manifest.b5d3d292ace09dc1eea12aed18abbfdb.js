dfa7525aeb92916aff7071a3aaf8d046a43cc331
MIGHOST <mi.sidorenko@gmail.com>
2020-03-27 22:16:26 +0200

    Updates

---
+++ precache-manifest.b5d3d292ace09dc1eea12aed18abbfdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff4db2b08e607bb5ced8a6eb75dd2d7",
+    "url": "/goit-react-hw-01-component/index.html"
+  },
+  {
+    "revision": "be39b22e8deda8114b55",
+    "url": "/goit-react-hw-01-component/static/css/main.b73a6dd2.chunk.css"
+  },
+  {
+    "revision": "dc49b0cc17af5443aaaa",
+    "url": "/goit-react-hw-01-component/static/js/2.153fe786.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-01-component/static/js/2.153fe786.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be39b22e8deda8114b55",
+    "url": "/goit-react-hw-01-component/static/js/main.47753c4b.chunk.js"
+  },
+  {
+    "revision": "92ccaaddc18ac26e2b73",
+    "url": "/goit-react-hw-01-component/static/js/runtime-main.93b5ae32.js"
+  }
+]);
